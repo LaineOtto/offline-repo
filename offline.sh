364a2303e852ff0ec21edd65aa446a6ed242b0e0
Cacoethes <12479475+LaineOtto-Rognlie@users.noreply.github.com>
2019-01-15 12:05:41 -0800

    Clarified Comments

--- offline.sh
+++ offline.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-httpip="192.168.12.128" #set to the ip of the repo vm
+httpip="192.168.12.128" #set to the ip of the machine you ran online.sh on
 echo "deb [trusted=yes arch=amd64] http://$httpip/debs/ /" > /etc/apt/sources.list.d/offline.list
 rm -f /etc/apt/sources.list
 apt update
